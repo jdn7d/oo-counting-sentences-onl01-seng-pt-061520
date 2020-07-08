6752ae65cbb211d6b35a42b562d107bd5c222553
Jessie Donovan <donovanajessie@gmail.com>
2020-07-08 01:27:35 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -4,30 +4,34 @@ class String
 
   def sentence?
     if self.end_with?(".")
-      return true
-    else
-      return false
-    end
+    return true
+  else 
+    return false
+  end
   end
 
   def question?
+
     if self.end_with?("?")
-      return true
-    else
-      return false
+    return true
+  else 
+    return false
   end
-end
+  end
+
+  end
 
   def exclamation?
     if self.end_with?("!")
-      return true
-    else
-      return false
-    end
+    return true
+  else 
+    return false
+  end
+  end
   end
 
   def count_sentences
-    self.split(/\./\?/\!/)
+    self.split(/\.|\?|\!/).delete_if {|word| word.size <= 1}.size
     binding.pry
-
+  end
 end
